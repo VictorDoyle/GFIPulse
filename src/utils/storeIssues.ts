428d950cfdfe0fa977951970a193b14e300b69ad
VictorDoyle <victor-doyle@outlook.com>
2024-11-26 19:56:26 -0500

    change typo

--- src/utils/storeIssues.ts
+++ src/utils/storeIssues.ts
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const storageFilePath = path.resolve(__dirname, '../../data/fetchedissues.json');
+const storageFilePath = path.resolve(__dirname, '../../data/fetche_issues.json');
 
 export function readFetchedIssues(): Set<string> {
   if (fs.existsSync(storageFilePath)) {
